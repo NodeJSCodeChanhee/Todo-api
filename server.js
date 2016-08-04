b49c898166738fc9467c1ce3c64ff796924c9e9b
InChan Hwang <neogeoss1@gmail.com>
2016-08-04 19:58:26 +0900

    Add post for todos

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id : 1, 
-	description : 'Meet mom for lunch',
-	completed : false
-}, { 
-	id: 2,
-	description : 'Go to market',
-	completed : false
-}, {
-	id: 3,
-	description : 'Get a girlfriend!',
-	completed : true
-}];
+var todos = [];
+var todoNextId = 1;
+
+
+app.use(bodyParser.json());
+
 
 app.get('/', function(req, res){
 	res.send('Todo API Root');
@@ -49,6 +43,22 @@ app.get('/todos/:id', function(req, res){
 
 	res.send('Asking for todos with id of ' + req.params.id);
 	res.end();
+});
+
+
+//POST /todos 
+app.post('/todos', function(req, res){
+	var body = req.body;
+	//add id field
+	body.id = todoNextId;
+	todoNextId += 1;
+
+	//push body into array.
+	todos.push(body);
+
+	console.log('description:' + body.description);
+
+	res.json(body);
 });
 
 
