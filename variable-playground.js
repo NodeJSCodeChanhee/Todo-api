e67df334c96fb3716be89c23328da8f50cc01158
InChan Hwang <neogeoss1@gmail.com>
2016-08-05 19:13:30 +0900

    Add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,45 @@
+// var person = {
+// 	name : 'Inchan Hwang',
+// 	age : 21
+// };
+
+// function updatePerson (obj){
+// 	// obj = {
+// 	// 	name : 'Fred',
+// 	// 	age : 24
+// 	// };
+// 	obj.age = 24;
+// 	obj.name = 'Fred';
+// }
+
+// updatePerson(person);
+
+// console.log(person);
+
+// Array Example 
+// var array = ['inchan','is genious', 'king of programming'];
+
+// function falseUpdate(obj){
+// 	obj = [23, 23, 43];
+// }
+
+// function trueUpdate(obj){
+// 	obj[0] = 343;
+// }
+
+// // falseUpdate(array);
+// trueUpdate(array);
+
+// console.log(array);
+
+var grades = [14, 88];
+
+function addGrades(gradesArr){
+	gradesArr.push(55);
+	debugger;
+
+}
+
+addGrades(grades);
+
+console.log(grades);
