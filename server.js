bd0f75a1cdd3eeb89a099cd102fead37a4988eb2
InChan Hwang <neogeoss1@gmail.com>
2016-08-09 21:38:39 +0900

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -165,18 +165,35 @@ app.post('/todos', function(req, res) {
 app.delete('/todos/:id', function(req, res) {
 
 	var todoId = parseInt(req.params.id, 10);
-	var matchedEle = _.findWhere(todos, {
-		id: todoId
-	});
+	
+    db.todo.destroy({
+   		where : {
+    		id : todoId
+    	}
+    }).then(function(rowsDeleted){
+    	if(rowsDeleted === 0){
+    		res.status(404).json({
+    			error : 'No todo with id'
+    		});
+    	} else {
+    		res.status(204).send();
+    	}
+    }, function(){
+    	res.status(500).send();
+    });
 
-	if (!matchedEle) {
-		res.status(404).json({
-			"error": "no todo found ! "
-		});
-	} else {
-		todos = _.without(todos, matchedEle);
-		res.json(matchedEle);
-	}
+	// var matchedEle = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+
+	// if (!matchedEle) {
+	// 	res.status(404).json({
+	// 		"error": "no todo found ! "
+	// 	});
+	// } else {
+	// 	todos = _.without(todos, matchedEle);
+	// 	res.json(matchedEle);
+	// }
 });
 
 //PUT /todos/:id
