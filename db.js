e872abfd8fd3f5a54f42b58c5697e8170092cfbb
InChan Hwang <neogeoss1@gmail.com>
2016-08-15 21:38:29 +0900

    toLowerCase() was the error. Capitalization must be minded next time

--- db.js
+++ db.js
@@ -1,27 +1,23 @@
 var Sequelize = require('sequelize');
-
 var env = process.env.NODE_ENV || 'development';
-
 var sequelize;
 
-if(env === 'production'){
+if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		dialect : 'postgres'
+		dialect: 'postgres'
 	});
 } else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
-		'dialect' : 'sqlite',
-		'storage' : __dirname + '/data/dev-todo-api.sqlite'
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
 	});
 }
-
 
 var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.user = sequelize.import(__dirname + '/models/user.js');
 db.sequelize = sequelize;
-
 db.Sequelize = Sequelize;
 
 module.exports = db;
--- models/user.js
+++ models/user.js
@@ -1,30 +1,29 @@
 var bcrypt = require('bcrypt');
-
 var _ = require('underscore');
 
-module.exports = function(sequelize, DataTypes){
+module.exports = function (sequelize, DataTypes) {
 	return sequelize.define('user', {
-		email : {
+		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
-			unique : true,
+			unique: true,
 			validate: {
-				isEmail : true
+				isEmail: true
 			}
 		},
-		salt : {
-			type : DataTypes.STRING
+		salt: {
+			type: DataTypes.STRING
 		},
-		password_hash : {
-			type : DataTypes.STRING
+		password_hash: {
+			type: DataTypes.STRING
 		},
-		password : {
-			type : DataTypes.VIRTUAL,
-			allowNull : false,
-			validate : {
+		password: {
+			type: DataTypes.VIRTUAL,
+			allowNull: false,
+			validate: {
 				len: [7, 100]
 			},
-			set: function(value){
+			set: function (value) { 
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
 
@@ -32,17 +31,21 @@ module.exports = function(sequelize, DataTypes){
 				this.setDataValue('salt', salt);
 				this.setDataValue('password_hash', hashedPassword);
 			}
-
 		}
 	}, {
-		hooks : {
-			beforeValidate : function(user){
-				//user.email = user.email.toLowercase();
-				if(typeof user.email === 'string'){
-					user.email = user.email.toLowercase();
+		hooks: {
+			beforeValidate: function (user, options) {
+				// user.email
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
 				}
-
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function () {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			}
 		}
 	});
-}
+};
