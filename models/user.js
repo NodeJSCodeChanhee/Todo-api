65e78dc9fb834f19ed01775a41595c034e6f0d9d
InChan Hwang <neogeoss1@gmail.com>
2016-08-18 12:00:32 +0900

    Modification on user.js model and

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
 module.exports = function (sequelize, DataTypes) {
-	return sequelize.define('user', {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -41,6 +41,32 @@ module.exports = function (sequelize, DataTypes) {
 				}
 			}
 		},
+		classMethods:{
+			authenticate : function(body){
+				return new Promise(function (resolve, reject){
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+
+					}
+
+
+
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
+				});
+			}
+		},
 		instanceMethods: {
 			toPublicJSON: function () {
 				var json = this.toJSON();
@@ -48,4 +74,5 @@ module.exports = function (sequelize, DataTypes) {
 			}
 		}
 	});
+	return user;
 };
--- server.js
+++ server.js
@@ -257,28 +257,33 @@ app.post('/users', function(req, res){
 app.post('/users/login', function(req, res){
 	var body = _.pick(req.body, 'email', 'password');
 
-	var where = {};
+	db.user.authenticate(body).then(function(user){
+		res.json(user.toPublicJSON());
+	}, function(){
+		res.status(401).send();
+	});
+	// var where = {};
 
-	if(typeof body.email !== 'string' || typeof body.password !== 'string'){
-		return res.status(400).send();
+	// if(typeof body.email !== 'string' || typeof body.password !== 'string'){
+	// 	return res.status(400).send();
 		
-	}	
+	// }	
 
 	
 
-	db.user.findOne({
-		where : {
-			email : body.email
-		}
-	}).then(function(user){
-		if(!user || !bcrypt.compareSync(body.password, user.get('password_hash'))){
-			return res.status(401).send();
-		}
+	// db.user.findOne({
+	// 	where : {
+	// 		email : body.email
+	// 	}
+	// }).then(function(user){
+	// 	if(!user || !bcrypt.compareSync(body.password, user.get('password_hash'))){
+	// 		return res.status(401).send();
+	// 	}
 
-		res.json(user.toPublicJSON());
-	}, function(e){
-		res.status(500).send();
-	});
+	// 	res.json(user.toPublicJSON());
+	// }, function(e){
+	// 	res.status(500).send();
+	// });
 
     
 
