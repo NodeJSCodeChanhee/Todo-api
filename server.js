82a7252c7b9d4f5160ccae3df5ada26ad743d773
InChan Hwang <neogeoss1@gmail.com>
2016-08-18 15:32:43 +0900

    Add login routeand authenticate class method

--- server.js
+++ server.js
@@ -264,32 +264,13 @@ app.post('/users/login', function(req, res){
 	});
 	// var where = {};
 
-	// if(typeof body.email !== 'string' || typeof body.password !== 'string'){
-	// 	return res.status(400).send();
-		
-	// }	
-
 	
-
-	// db.user.findOne({
-	// 	where : {
-	// 		email : body.email
-	// 	}
-	// }).then(function(user){
-	// 	if(!user || !bcrypt.compareSync(body.password, user.get('password_hash'))){
-	// 		return res.status(401).send();
-	// 	}
-
-	// 	res.json(user.toPublicJSON());
-	// }, function(e){
-	// 	res.status(500).send();
-	// });
 
     
 
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
